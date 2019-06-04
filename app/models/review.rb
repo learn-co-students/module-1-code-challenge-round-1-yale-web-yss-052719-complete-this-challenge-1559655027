1ae4697d6f201d000d73af29d531e45e5f5a59d9
maxsunnyday <max.sun@yale.edu>
2019-06-04 10:27:01 -0400

    Done

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,26 @@
 class Review
-  
+    attr_reader :customer, :restaurant, :rating, :content
+
+    @@all = []
+
+    def initialize(customer, restaurant, rating, content)
+        @customer = customer
+        @restaurant = restaurant
+
+        if rating > 5
+            @rating = 5
+        elsif rating < 1
+            @rating = 1
+        else
+            @rating = rating
+        end
+        
+        @content = content
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 end
 
