5ba677c6fd3d39c6eb521d35dc6a11d3daa3b611
github username <github email address>
2019-06-04 10:08:48 -0400

    done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,63 @@
 class Customer
   attr_reader :first_name, :last_name
-
+  @@all = []
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def self.all
+    @@all
+  end
+
+  def add_review(restaurant, content, rating)
+    Review.new(self, restaurant, content, rating)
+  end
+
+  #self method on all rev instances by this customer
+  def reviews
+    Review.all.select do |review|
+      review.customer == self
+    end
+  end
+
+  def num_reviews
+    self.reviews.length
+  end
+
+  def restaurants
+    self.reviews.map do |rev| # arr of restuarants that customer has reviewed
+      rev.restaurant
+    end.uniq
+  end
+
+  def self.find_by_name(name)
+    temp = name.split(" ")
+    self.all.find do |customer|
+      customer.first_name == temp[0] && customer.last_name == temp[1] 
+    end
+  end
+
+  def self.find_all_by_first_name(name)
+    self.all.select do |customer|
+      customer.first_name == name
+    end
+  end
+
+  def self.all_names
+    arr = []
+    self.all.each do |customer|
+      temp = [customer.first_name, customer.last_name].reject(&:empty?).join(' ')
+      # temp = customer.first_name.concat(customer.last_name)
+      arr << temp
+    end
+    arr
+  end
+
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,61 @@
 class Restaurant
   attr_reader :name
-
+  @@all = []
   def initialize(name)
     @name = name
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+
+  #self made method - return all rev instance for res
+  def reviews
+    Review.all.select do |rev|
+      rev.restaurant == self
+    end
+  end
+
+  def customers
+    self.reviews.map do |rev|
+      rev.customer
+    end.uniq
+  end
+
+  # def reviews
+  #   self.rev.map do |rev|
+  #     rev.restaurant
+  #   end
+  # end
+
+  def average_star_rating
+    num = 0
+    num1 = 0
+    self.reviews.each do |rev|
+      num += rev.rating
+      num1 += 1
+    end
+    num / num1
+  end
+
+  def longest_review
+    hold = ""
+    self.reviews.each do |rev|
+      if hold.length < rev.content.length
+        hold = rev.content
+      end
+    end
+    hold
+  end
+
+  def self.find_by_name(name)
+    self.all.find do |rest|
+      rest.name == name
+    end
   end
 
 end
+
