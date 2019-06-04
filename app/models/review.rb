c4650a857c7aeee0618d53410fbd8cb31cc0de66
Roxanne Harris <roxanne.harris@yale.edu>
2019-06-04 10:35:45 -0400

    finished code challenge

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,24 @@
 class Review
+
+    @@all = []
+    attr_reader :customer, :restaurant
+    attr_accessor :content, :rating
+
+    def initialize(customer, restaurant, rating, content)
+        @customer = customer
+        @restaurant = restaurant
+        rating >= 1 && rating <= 5 ? @rating = rating : nil
+        rating < 1 ? @rating = 1 : @rating = 5
+        @content = content
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+
   
 end
 
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,26 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+c_names = [["Roxanne", "Harris"], ["Naruto", "Uzumaki"]]
+
+r_names = ["Pepe's Pizza", "Yorkside", "Brick Oven", "Alpha Delta", "Shake Shack", "Vivi's"]
+
+content = ["You suck!", "You rock!", "I'm satisifed.", "OK", "Sorta OK", "Flat out DUB..."]
+customers = []
+restaurants = []
+(0..10).each do |i|
+  c_names[i] ? customers << Customer.new(c_names[i][0], c_names[i][1]) : nil
+  r_names[i] ? restaurants << Restaurant.new(r_names[i]) : nil
+end
+
+# Add reviews
+(0..10).each do |i|
+  customers.sample.add_review(restaurants.sample, "You suck!", 6)
+end
+
+a = restaurants.map {|r| r.reviews }
+
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
