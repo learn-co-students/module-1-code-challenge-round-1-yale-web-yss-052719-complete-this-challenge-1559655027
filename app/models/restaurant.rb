f2cd85ba671f246acb55b5f42c48328bd012d422
Matthew Peoples <matthew.peoples@yale.edu>
2019-06-04 10:34:36 -0400

    time up

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,54 @@
 class Restaurant
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def customers
+    Review.all.select do |rest|
+      rest.restaurant == self
+    end.map do |cust|
+      cust.customer
+    end.uniq
+  end
+
+  def reviews
+    Review.all.select do |find|
+      find.restaurant == self
+    end
+  end
+
+  def average_star_rating
+    sum = 0
+    self.reviews.each do |star|
+      sum += star.rating
+    end
+    sum * 1.0 / self.reviews.count
+  end
+
+  def longest_review
+    rev = self.reviews.map do |rate|
+      rate.content
+    end
+      # Used https://stackoverflow.com/questions/22438646/
+      # how-can-i-select-the-longest-string-from-a-ruby-array
+      # for guidance
+      rev.max_by(&:length)
+  end
+
+  def self.find_by_name(name)
+    self.all.select do |this_name|
+      this_name.name == name
+    end
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,11 +1,28 @@
 require_relative '../config/environment.rb'
+require_relative '../app/models/customer.rb'
+require_relative '../app/models/restaurant.rb'
+require_relative '../app/models/review.rb'
 
 def reload
   load 'config/environment.rb'
 end
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+
+matt = Customer.new("Matt", "Peoples")
+jodie = Customer.new("Jod", "Coburn")
+prof_x = Customer.new("Charles", "Xavier")
+charlie_x = Customer.new("Charles", "Xiggy")
+
+mc_d = Restaurant.new("McDonalds")
+bk = Restaurant.new("Burger King")
+kfc = Restaurant.new("KFC")
+
+matt.add_review(kfc, "Didnt like it", 1)
+
+review1 = Review.new(matt, mc_d, 4, "Im loving it")
+review2 = Review.new(matt, kfc, 3, "Its ok")
+review3 = Review.new(jodie, kfc, 2, "Hate it")
+review4 = Review.new(jodie, bk, 4, "Im not a vegetarian anymore")
+review5 = Review.new(prof_x, mc_d, 1, "Im not amused, not one bit")
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
