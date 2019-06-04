6c25dd9aa068ac9e1efb0ecb74f3d373e1af5959
Alexandro Alanya <alexandro.alanya@gmail.com>
2019-06-04 10:36:00 -0400

    -

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,4 +5,18 @@ class Restaurant
     @name = name
   end
 
+  def customers = []
+
+  end
+
+  def reviews
+  end
+
+  def average_star_rating
+  end
+
+  def longest_review
+  end
+
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,22 @@
 class Review
-  
-end
 
+  attr_reader :customer, :restaurant, :rating, :content)
+
+  @@all << self
+
+  def customers
+  end
+
+  def restaurants
+  end
+
+  def rating [0..4]
+    
+  end
+
+  def content
+  end
+
+
+
+end
